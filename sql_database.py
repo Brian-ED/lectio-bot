320742d83f40bc8499ea0a25d25eba823c9e8ce6
Brian E <72316548+Brian-ED@users.noreply.github.com>
2025-05-02 00:59:51 +0200

    removed leading whitespace in sql_database.py

--- sql_database.py
+++ sql_database.py
@@ -70,12 +70,8 @@ def uploadToTable():
     mincursor = makeCursor(mindb)
     for i in range(len(names)):
         sql = f"INSERT INTO {db_table} (titel, frist, elevtid, slettet) VALUES (%s, %s, %s, False)"
-        val = (names[i], frister[i], elevtid[i])
-    
-        #print(val)
-        mincursor.execute(sql, val)
-        
-        mindb.commit()
+        val = (names[i], frister[i], elevtid[i])        #print(val)
+        mincursor.execute(sql, val)        mindb.commit()
     print("Data uploadet!")
     mincursor.close()
     mindb.close()
@@ -92,9 +88,7 @@ def showNext(limitval):
     resultater = []
 
     for x in resultat:
-        resultater.append(x)
-    
-    #print("Lektier optalt...")
+        resultater.append(x)    #print("Lektier optalt...")
     mincursor.close()
     mindb.close()
     #print(list(resultater[0]))
@@ -104,13 +98,9 @@ def showNext(limitval):
 # Slet gamle opgaver
 def deleteOld():
     mindb = connect()
-    mincursor = makeCursor(mindb)
-    
-    # Fjerner gamle lektier
+    mincursor = makeCursor(mindb)    # Fjerner gamle lektier
     sql = f"UPDATE {db_table} SET slettet = True WHERE frist < curtime() AND slettet = False"
-    #sql = f"DELETE FROM {db_table} WHERE frist < curtime()"
-    
-    mincursor.execute(sql)
+    #sql = f"DELETE FROM {db_table} WHERE frist < curtime()"    mincursor.execute(sql)
     mindb.commit()
 
     # Fjerner duplicates -- Beholder den lektier med lavest ID
